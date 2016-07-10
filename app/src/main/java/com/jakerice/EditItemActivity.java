e3155bfb67ff702709d3ee2d91f70ec08d3649d7
Jake Rice <jrice189@gmail.com>
2016-07-10 15:47:31 -0700

    Cleanup

---
+++ app/src/main/java/com/jakerice/EditItemActivity.java
@@ -0,0 +1,38 @@
+package com.jakerice.simpletodo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EditItemActivity extends AppCompatActivity {
+    EditText editItem;
+    Button btnUpdate;
+    String pos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+        editItem = (EditText) findViewById(R.id.editText);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        Intent intent = getIntent();
+        String extraText = intent.getStringExtra("itemText");
+        pos = intent.getStringExtra("itemPos");
+        editItem.setText(extraText);
+        editItem.setSelection(extraText.length());
+    }
+
+    public void onUpdate(View v) {
+        String newText = editItem.getText().toString();
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("itemText", newText);
+        resultIntent.putExtra("itemPos", pos);
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
+    }
+}
